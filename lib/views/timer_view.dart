3e5569427616d43f31ee6066d144fa886002c29f
Joscha <joschavonandrian@gmail.com>
2023-08-13 23:35:45 +0200

    add dnf

--- lib/views/timer_view.dart
+++ lib/views/timer_view.dart
@@ -4,12 +4,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:speedclimbing/providers/current_time_provider.dart';
 import 'package:speedclimbing/views/time_view.dart';
+import 'package:speedclimbing/widgets/flight_animation.dart';
 
-class TimerView extends ConsumerWidget {
+class TimerView extends ConsumerStatefulWidget {
   const TimerView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _TimerViewState();
+}
+
+class _TimerViewState extends ConsumerState<TimerView> {
+  bool isDNF = false;
+
+  @override
+  Widget build(BuildContext context) {
     Duration? currentTime = ref.watch(currentTimeProvider);
     return Center(
         child: currentTime != null
@@ -19,27 +27,47 @@ class TimerView extends ConsumerWidget {
                 children: [
                   Hero(
                     tag: 'timeLabel',
-                    child: Text(
-                        '${currentTime.inSeconds.toString().padLeft(2, '0')}.${currentTime.inMilliseconds.remainder(1000).toString().padLeft(3, '0')}',
-                        style: const TextStyle(
-                            fontSize: 52,
-                            fontFeatures: [
-                              FontFeature.tabularFigures(),
-                            ],
-                            color: Colors.greenAccent)),
+                    flightShuttleBuilder: flightShuttleBuilder,
+                    child: isDNF
+                        ? const Text(
+                            'DNF',
+                            style: TextStyle(fontSize: 52),
+                          )
+                        : Text(
+                            '${currentTime.inSeconds.toString().padLeft(2, '0')}.${currentTime.inMilliseconds.remainder(1000).toString().padLeft(3, '0')}',
+                            style: const TextStyle(
+                                fontSize: 52,
+                                fontFeatures: [
+                                  FontFeature.tabularFigures(),
+                                ],
+                                color: Colors.greenAccent)),
                   ),
                   const SizedBox(
                     height: 10,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                          onPressed: () {}, icon: const Icon(Icons.close)),
-                      IconButton(
-                          onPressed: () {}, icon: const Icon(Icons.block))
-                    ],
-                  ),
+                  isDNF
+                      ? IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isDNF = false;
+                            });
+                          },
+                          icon: const Icon(Icons.undo))
+                      : Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.close)),
+                            IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    isDNF = true;
+                                  });
+                                },
+                                icon: const Icon(Icons.block))
+                          ],
+                        ),
                   const SizedBox(
                     height: 100,
                   ),
